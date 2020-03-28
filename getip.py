54860eec09e24d4f6819f2c661b97f310393dddd
hpcex <hpcex53@gmail.com>
2020-03-28 20:57:24 +0800

    update

--- getip.py
+++ getip.py
@@ -3,7 +3,7 @@
 import socket
 
 def GetLocalIPByPrefix(prefix):
-    r""" 多网卡情况下，根据前缀获取IP（Windows 下适用） """
+    """ 多网卡情况下，根据前缀获取IP（Windows 下适用） """
     localIP = ''
     for ip in socket.gethostbyname_ex(socket.gethostname())[2]:
         if ip.startswith(prefix):
